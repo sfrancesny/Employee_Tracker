a30d5533c61576ca0640ef7cf3d99934d5f3e798
sfrancesny <134027005+sfrancesny@users.noreply.github.com>
2023-10-14 02:31:16 -0400

    changed seed data (manager_id)

--- db/seeds.sql
+++ db/seeds.sql
@@ -5,6 +5,7 @@ INSERT INTO department (name) VALUES ('Marketing'), ('Inventory'), ('HR'), ('IT'
 INSERT INTO role (title, salary, department_id) VALUES 
 ('Content Marketer', 71000, 1),
 ('Copywriter', 65000, 1),
+('Marketing Manager', 90000, 1),
 ('Inventory Manager', 78000, 2),
 ('HR Manager', 79500, 3),
 ('Systems Analyst', 93000, 4),
@@ -12,9 +13,10 @@ INSERT INTO role (title, salary, department_id) VALUES
 
 -- inserts sample data into the `employee` table
 INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES 
-('Blaire', 'Hart', 1, NULL),
-('Casen', 'Dudley', 2, NULL),
-('Zoie', 'Chavez', 3, 1),
-('Riley', 'Parrish', 4, 2),
+('Blaire', 'Hart', 1, 3),
+('Casen', 'Dudley', 2, 3),
+('Khaleesi', 'Nyen', 2, NULL),
+('Zoie', 'Chavez', 3, NULL),
+('Riley', 'Parrish', 4, NULL),
 ('Samson', 'Dennis', 5, NULL),
 ('Kem', 'Mackey', 6, NULL);
